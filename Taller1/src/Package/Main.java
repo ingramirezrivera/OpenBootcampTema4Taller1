76c30e5325d75eb6b985be4d5d91aaa9fa74b235
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-06-08 23:03:56 -0500

    While exercise

--- Taller1/src/Package/Main.java
+++ Taller1/src/Package/Main.java
@@ -3,7 +3,9 @@ package Package;
 public class Main {
     public static void main(String[] args) {
         int numeroIf = 0;
-        
+        int numeroWhile = -3;
+
+        System.out.println("---------------IF ELSE------------");
         if(numeroIf > 0){
             System.out.println("El número es positivo:");
         }else if(numeroIf < 0){
@@ -11,5 +13,13 @@ public class Main {
         }else{
             System.out.println("El número es cero.");
         }
+
+        System.out.println("---------------WHILE------------");
+        while(numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile += 1;
+        }
+
+
     }
 }
