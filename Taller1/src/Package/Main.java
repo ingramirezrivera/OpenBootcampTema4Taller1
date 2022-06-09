2ec0d777d54530fefbac6293dede1ff2b4526c1c
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-06-08 23:00:14 -0500

    If Else exercise

--- Taller1/src/Package/Main.java
+++ Taller1/src/Package/Main.java
@@ -2,6 +2,14 @@ package Package;
 
 public class Main {
     public static void main(String[] args) {
+        int numeroIf = 0;
         
+        if(numeroIf > 0){
+            System.out.println("El número es positivo:");
+        }else if(numeroIf < 0){
+            System.out.println("El número es negativo");
+        }else{
+            System.out.println("El número es cero.");
+        }
     }
 }
